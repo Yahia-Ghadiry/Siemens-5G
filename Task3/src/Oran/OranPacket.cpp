1e4fe02476570f0e42e1a4a952ef9ac58a497f1c
Yahia-Ghadiry <yahiaghadiry@gmail.com>
2024-09-28 20:16:27 +0300

    Made seconed constructor for OranPacket using OranOptions

--- Task3/src/Oran/OranPacket.cpp
+++ Task3/src/Oran/OranPacket.cpp
@@ -8,7 +8,7 @@ using std::fill;
 using std::pair;
 using std::invalid_argument;
 
-OranPacket::OranPacket(const uint8_t &SeqID, const uint8_t &FrameID, const uint8_t &SubFrameID, const uint8_t &SlotID, const uint8_t &SympolID, const uint16_t &PRBStart, const vector<pair<int8_t, int8_t>> &IQSamples)
+OranPacket::OranPacket(const uint8_t &SeqID, const uint8_t &FrameID, const uint8_t &SubFrameID, const uint8_t &SlotID, const uint8_t &SymbolID, const uint16_t &PRBStart, const vector<pair<int8_t, int8_t>> &IQSamples)
 {
 
     
@@ -52,11 +52,65 @@ OranPacket::OranPacket(const uint8_t &SeqID, const uint8_t &FrameID, const uint8
     fill(this->OranDirection_Version_fIndex, this->OranDirection_Version_fIndex + 1, 0x00);
     *(this->FrameID) = FrameID;
     *(this->SubFrameID_SlotIDp1) = (SubFrameID << 4) + ((SlotID >> 2) & 0b00001111);
-    *(this->SlotIDp2_SymbolID) = (SlotID << 6) + (SympolID & 0b00111111);
+    *(this->SlotIDp2_SymbolID) = (SlotID << 6) + (SymbolID & 0b00111111);
     
     copy(SectionID_rb_symlc_Configuration.begin(), SectionID_rb_symlc_Configuration.end(), this->SectionIDp1);
     *(this->SectionIDp2_rb_symlc_StartPRBUp1) += ((PRBStart >> 8) & 0b00000011); 
     *(this->StartPRBUp2) = (PRBStart & 0b11111111);
+    *(this->NumPRBUp) = NumPRBs;
+
+
+   FillIQ(IQSamples); 
+}
+
+
+
+OranPacket::OranPacket(const OranOptions &PacketInformation, const vector<pair<int8_t, int8_t>> &IQSamples)
+{
+
+    
+    uint8_t NumPRBs = IQSamples.size() / 12;
+    
+    if (NumPRBs * 12 != IQSamples.size())
+        throw invalid_argument("IQ samples need to be a multiple of 12 (Full RBs)");
+
+    Payload = vector<uint8_t>(eCPRIHeaderSize + OranHeadersSize + IQSamples.size() * 2); // Note: If I and Q use 2 bytes each need to adjuse
+
+    // Setting Iterators for each section
+    this->eCPRIVersion_Contacation = Payload.begin();
+    this->eCPRIMessageType = this->eCPRIVersion_Contacation + 1;
+    this->eCPRIPayloadSize = this->eCPRIMessageType + 1;
+    this->eCPRI_RTC = this->eCPRIPayloadSize + 2;
+    this->eCPRISeqID = this->eCPRI_RTC + 2;
+
+    this->OranDirection_Version_fIndex = this->eCPRISeqID + 2;
+    this->FrameID = this->OranDirection_Version_fIndex + 1;
+    this->SubFrameID_SlotIDp1 = this->FrameID + 1;
+    this->SlotIDp2_SymbolID = this->SubFrameID_SlotIDp1 + 1;
+    
+    this->SectionIDp1 = this->SlotIDp2_SymbolID + 1;
+    this->SectionIDp2_rb_symlc_StartPRBUp1 = this->SectionIDp1 + 1;
+    this->StartPRBUp2 = this->SectionIDp2_rb_symlc_StartPRBUp1 + 1;
+    this->NumPRBUp = this->StartPRBUp2 + 1;
+
+    this->IQSamples = this->NumPRBUp + 1;
+
+
+    // Setting everything in the correct bit postions
+    fill(this->eCPRIVersion_Contacation, this->eCPRIVersion_Contacation + 1, 0x00);
+    copy(eCPRIType_IQdata.begin(), eCPRIType_IQdata.end(), this->eCPRIMessageType);
+    *(this->eCPRIPayloadSize) = OranHeadersSize + IQSamples.size() * 2; 
+    fill(this->eCPRI_RTC, this->eCPRI_RTC + 2, 0x00);
+    *(this->eCPRISeqID) = PacketInformation.SeqID;
+
+    fill(this->OranDirection_Version_fIndex, this->OranDirection_Version_fIndex + 1, 0x00);
+    *(this->FrameID) = PacketInformation.FrameID;
+    *(this->SubFrameID_SlotIDp1) = (PacketInformation.SubFrameID << 4) + ((PacketInformation.SlotID >> 2) & 0b00001111);
+    *(this->SlotIDp2_SymbolID) = (PacketInformation.SlotID << 6) + (PacketInformation.SymbolID & 0b00111111);
+    
+    copy(SectionID_rb_symlc_Configuration.begin(), SectionID_rb_symlc_Configuration.end(), this->SectionIDp1);
+    *(this->SectionIDp2_rb_symlc_StartPRBUp1) += ((PacketInformation.PRBStart >> 8) & 0b00000011); 
+    *(this->StartPRBUp2) = (PacketInformation.PRBStart & 0b11111111);
     *(this->NumPRBUp) = NumPRBs;
 
 
